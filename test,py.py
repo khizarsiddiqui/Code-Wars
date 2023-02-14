e66c667fbcc24e095bfd93fb2a5444d7af4c76ae
khizarsiddiqui <khizarsiddiqui10@gmail.com>
2023-02-15 01:38:12 +0500

    15/02

--- test,py.py
+++ test,py.py
@@ -61,3 +61,47 @@
 
 # s = 'abcdefg'
 # print(solution(s))
+
+# In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+# pairs = {'A':'T','T':'A','C':'G','G':'C'}
+# def DNA_strand(dna):
+#     return ''.join([pairs[x] for x in dna])
+
+# dna = "ATTGC"
+# print(DNA_strand(dna))
+
+
+# An ATM ran out of 10 dollar bills and only has 100, 50 and 20 dollar bills. Given an amount between 40 and 10000 dollars (inclusive) 
+# and assuming that the ATM wants to use as few bills as possible,  determinate the minimal number of 100, 50 and 20 dollar bills the ATM needs to dispense (in that order).
+
+# def withdraw(n):
+#     n50 = 0
+#     n20, r = divmod(n, 20)
+#     # print(n20,r)
+#     if r == 10:
+#         n20 -= 2
+#         n50 += 1
+#     n100, n20 = divmod(n20, 5)
+#     return [n100, n50, n20]
+# n = (60)
+# print (withdraw(n))
+
+
+
+# Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+# def maskify(cc):
+#     strlen=len(cc)
+#     str1 = cc.replace(cc, '#' * strlen)
+#     str1 = str1[:-4]
+#     # print(str1)
+#     str2=cc[-4:]  
+#     # print(str2)
+#     return "".join([str1,str2])
+
+
+# def maskify2(cc):
+#     return "#"*(len(cc)-4) + cc[-4:]
+
+# cc = "SF$SDfgsd2eA"
+# print(maskify2(cc))
